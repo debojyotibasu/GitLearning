e9508464519c4828b1401943ba3be8c6e7a904f8
Debojyoti Basu <djbasu2010@gmail.com>
2021-12-10 21:22:12 +0530

    added message in addNumbers.java

--- addNumbers.java
+++ addNumbers.java
@@ -4,8 +4,8 @@ public class addNumbers {
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 20;
-		
-		System.out.println(a+b);
+		int c = a + b;
+		System.out.println("Total value of two numbers: " + c);
 	}
 
 }
