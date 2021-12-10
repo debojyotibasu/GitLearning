1f04c8db74f2e0b29b5d668918f75765846def00
Debojyoti Basu <djbasu2010@gmail.com>
2021-12-10 21:20:35 +0530

    adding minusNumbers.java

---
+++ minusNumbers.java
@@ -0,0 +1,13 @@
+
+public class minusNumbers {
+
+	public static void main(String[] args) {
+		int a = 40;
+		int b = 20;
+		int c = a - b;
+		
+		System.out.println("Substraction value of two numbers: " + c);
+
+	}
+
+}
